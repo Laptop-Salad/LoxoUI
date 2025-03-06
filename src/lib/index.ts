69bc2cce3dc0a5fbe9d0faecbb401825e626f43d
Laptop-Salad <awallis1609@gmail.com>
2025-03-06 11:02:50 +0000

    fix: imports withou

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,12 +1,12 @@
 // place files you want to import through the `$lib` alias in this folder.
-import Button from "$lib/Button.svelte";
-import Pill from "$lib/Pill.svelte";
+import Button from "./Button.svelte";
+import Pill from "./Pill.svelte";
 
-import Drawer from "$lib/nav/drawer/Drawer.svelte";
-import DrawerTab from "$lib/nav/drawer/DrawerTab.svelte";
+import Drawer from "./nav/drawer/Drawer.svelte";
+import DrawerTab from "./nav/drawer/DrawerTab.svelte";
 
-import Bottom from "$lib/nav/bottom/Bottom.svelte";
-import BottomTab from "$lib/nav/bottom/BottomTab.svelte";
+import Bottom from "./nav/bottom/Bottom.svelte";
+import BottomTab from "./nav/bottom/BottomTab.svelte";
 
 export {
     Button,
